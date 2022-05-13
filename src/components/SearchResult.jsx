29ccff783b43520240ad96a164ebd4cf0c108a51
Ando Avetisyan <andoavetis0127@gmail.com>
2022-05-13 17:06:54 +0400

    SearchResult: get respons from request and use

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,22 +1,21 @@
 import React, { useState, useEffect } from "react";
-import { useLocation, useNavigate } from "react-router";
+import { useNavigate, useParams } from "react-router";
 //consts
-import { searchAPI, key } from "../helpers/consts";
+import { searchAPI, key, imgAPI } from "../helpers/consts";
 //styles
 import s from "../styles/SearchResult.module.scss";
 
 export default function SearchResult() {
   //react hooks
-  const location = useLocation();
   const navigate = useNavigate();
 
-  const movie_name = location.pathname.split("/")[2];
+  const { movieTitle } = useParams();
 
   const [movies, setMovies] = useState([]);
   useEffect(() => {
-    fetch(`${searchAPI}search/movie?api_key=${key}&query=${movie_name}`)
+    fetch(`${searchAPI}search/movie?api_key=${key}&query=${movieTitle}`)
       .then(res => res.json())
-      .then(results => setMovies(results));
+      .then(results => setMovies(results.results));
   }, []);
   console.log(movies);
   return (
@@ -28,9 +27,18 @@ export default function SearchResult() {
           src="https://img.icons8.com/ios-filled/50/000000/left.png"
           alt="back_btn"
         />
-        <h3 className={s.search_result_back_title}>{movie_name}</h3>
+        <h3 className={s.search_result_back_title}>{movieTitle}</h3>
       </div>
-      <div className={s.movie_result}></div>
+      <div className={s.movie_result}>
+        {movies.map(elem => {
+          return (
+            <div className={s.movie_result_card}>
+              <img className={s.movie_result_card_img} src={imgAPI + elem.poster_path} alt="filmPoster" />
+              <h3 className={s.movie_result_card_title}>{elem.title}</h3>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
