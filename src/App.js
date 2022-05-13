2a84106f700b1c25083fa428c5157dc13180992a
Ando Avetisyan <andoavetis0127@gmail.com>
2022-05-13 12:16:41 +0400

    Created a SearchPage component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+//react hooks
+import { Route, Routes } from "react-router";
+//componets
+import SearchPage from "./components/SearchPage";
+//styles
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello Ando</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<SearchPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
