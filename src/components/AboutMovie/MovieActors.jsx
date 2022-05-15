085815bc45bdebfe941a94ba75b850e538d7c885
Ando Avetisyan <andoavetis0127@gmail.com>
2022-05-15 22:24:23 +0400

    Actors component complited

--- src/components/AboutMovie/MovieActors.jsx
+++ src/components/AboutMovie/MovieActors.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+//constants
+import { imgAPI } from "../../helpers/consts";
+//images
+import noImg from "../../public/images/noImg.jpg";
+//styles
+import s from "../../styles/Actors.module.scss";
 
-export default function MovieActors() {
-  return <div>MovieActors</div>;
+export default function MovieActors({ actors }) {
+  const actorsNames = actors?.cast;
+  return (
+    <div className={s.movie_actors}>
+      {actorsNames?.map(element => (
+        <div className={s.movie_actors_cont}>
+          <img
+            className={s.movie_actors_cont_img}
+            src={element.profile_path ? imgAPI + element.profile_path : noImg}
+            alt="actors_img"
+          />
+          <div className={s.movie_actors_cont_div}>
+            <p className={s.movie_actors_cont_div_name}>{element.name}</p>
+            <p className={s.movie_actors_cont_div_char}>{element.character}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
--- src/pages/AboutMovie.jsx
+++ src/pages/AboutMovie.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useLayoutEffect, useState } from "react";
 import { useParams } from "react-router";
 //components
 import Loading from "../components/Loading";
 import MoviePreview from "../components/AboutMovie/MoviePreview";
+import MovieActors from "../components/AboutMovie/MovieActors";
 import PagesHeder from "../components/PagesHeder";
+
 //consts
 //consts
 import { searchAPI, key, imgAPI } from "../helpers/consts";
@@ -13,21 +15,43 @@ import s from "../styles/AboutMovie.module.scss";
 export default function AboutMovie() {
   const { movieId } = useParams();
   //state
-  const [aboutFilm, setAboutFilm] = useState();
+  // const [aboutFilm, setAboutFilm] = useState([]);
+  // const [actors, setActors] = useState([]);
+  // const [movieTrailers, setMovieTrailers] = useState([]);
+  const [movieInfo, setMovieInfo] = useState(null);
   const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    fetch(`${searchAPI}movie/${movieId}?api_key=${key}`)
-      .then(res => res.json())
-      .then(results => {
-        setAboutFilm(results);
-        setLoading(true);
-      });
+
+  useLayoutEffect(() => {
+    fetchAllInfo();
   }, []);
+  // useEffect(() => {
+  //   fetch(`${searchAPI}movie/${movieId}?api_key=${key}`)
+  //     .then(res => res.json())
+  //     .then(results => {
+  //       setAboutFilm(results);
+  //       setLoading(true);
+  //     });
+  // }, []);
+
+  async function fetchAllInfo() {
+    const [aboutFilm, actors, movieTrailers] = await Promise.all([
+      fetch(`${searchAPI}movie/${movieId}?api_key=${key}`).then(res => res.json()),
+      fetch(`${searchAPI}movie/${movieId}/credits?api_key=${key}`).then(res => res.json()),
+      fetch(`${searchAPI}movie/${movieId}/videos?api_key=${key}`).then(res => res.json())
+    ]);
+    setMovieInfo({
+      aboutFilm,
+      actors,
+      movieTrailers
+    });
+    setLoading(true);
+  }
 
   return (
     <div className={s.about_movie}>
-      <PagesHeder movieTitle={aboutFilm?.title} />
-      {loading ? <MoviePreview aboutFilm={aboutFilm} /> : <Loading />}
+      <PagesHeder movieTitle={movieInfo?.aboutFilm.title} />
+      {loading ? <MoviePreview aboutFilm={movieInfo?.aboutFilm} /> : <Loading />}
+      <MovieActors actors={movieInfo?.actors} />
     </div>
   );
 }
