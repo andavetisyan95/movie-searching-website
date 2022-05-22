973d35f3669deda217bbf7a283a1269701e2ff91
Ando Avetisyan <andoavetis0127@gmail.com>
2022-05-22 19:06:57 +0400

    MovieActors: add ternary operator for showing actors div

--- src/components/AboutMovie/MovieActors.jsx
+++ src/components/AboutMovie/MovieActors.jsx
@@ -12,19 +12,25 @@ export default function MovieActors({ actors }) {
 
   return (
     <div className={s.movie_actors}>
-      {actorsNames?.map(({ id, profile_path, name, character }) => (
-        <div key={id} className={s.movie_actors_cont}>
-          <img
-            className={s.movie_actors_cont_img}
-            src={profile_path ? REACT_APP_IMG_API + profile_path : noImg}
-            alt="actors_img"
-          />
-          <div className={s.movie_actors_cont_div}>
-            <p className={s.movie_actors_cont_div_name}>{name}</p>
-            <p className={s.movie_actors_cont_div_char}>{character}</p>
+      {actorsNames.length !== 0 ? (
+        actorsNames?.map(({ id, profile_path, name, character }) => (
+          <div key={id} className={s.movie_actors_cont}>
+            <img
+              className={s.movie_actors_cont_img}
+              src={profile_path ? REACT_APP_IMG_API + profile_path : noImg}
+              alt="actors_img"
+            />
+            <div className={s.movie_actors_cont_div}>
+              <p className={s.movie_actors_cont_div_name}>{name}</p>
+              <p className={s.movie_actors_cont_div_char}>{character}</p>
+            </div>
           </div>
+        ))
+      ) : (
+        <div className={s.noInfo}>
+          <p className={s.noInfo_p}>There is no info to show</p>
         </div>
-      ))}
+      )}
     </div>
   );
 }
